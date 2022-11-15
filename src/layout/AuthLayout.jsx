f0f342d11809e52ae2a1ecd96619fa97aa5a937e
Alex <santiago15422@gmail.com>
2022-11-15 02:09:23 -0500

    commit front 2

---
+++ src/layout/AuthLayout.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react"
+import {Outlet} from 'react-router-dom';
+
+const AuthLayout = () => {
+  return (
+    <>
+        <h1>Desde Auth</h1>
+        <h2>Formulario aqui</h2>
+        <main className='container mx-auto md:grid md:grid-cols-2 mt-12 gap-10 p-5 items-center'>
+        <Outlet />
+        </main>
+    </>
+  )
+}
+
+export default AuthLayout
